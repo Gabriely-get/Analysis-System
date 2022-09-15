ff1a949d6af1ca60ec05651cb71933c422201bd4
ilegra <gabrielysantos.informatica@gmail.com>
2022-09-14 22:43:03 -0300

    Add last files

--- src/main/java/com/gabrielyget/challenge/backend/AnalysisSystem/AnalysisSystemApplication.java
+++ src/main/java/com/gabrielyget/challenge/backend/AnalysisSystem/AnalysisSystemApplication.java
@@ -1,34 +1,28 @@
-package com.gabriely.challenge.backend.AnalysisSystem;
+package com.gabrielyget.challenge.backend.AnalysisSystem;
 
-import com.gabriely.challenge.backend.AnalysisSystem.Services.DataType.CustomerService;
-import com.gabriely.challenge.backend.AnalysisSystem.Services.DataType.SaleService;
-import com.gabriely.challenge.backend.AnalysisSystem.Services.DataType.SalesmanService;
-import com.gabriely.challenge.backend.AnalysisSystem.Services.File.ReportFileService;
-import com.gabriely.challenge.backend.AnalysisSystem.Services.File.SplitFileService;
-import com.gabriely.challenge.backend.AnalysisSystem.Services.Watch.WatchService;
+import com.gabrielyget.challenge.backend.AnalysisSystem.Services.DataType.CustomerService;
+import com.gabrielyget.challenge.backend.AnalysisSystem.Services.DataType.SaleService;
+import com.gabrielyget.challenge.backend.AnalysisSystem.Services.DataType.SalesmanService;
+import com.gabrielyget.challenge.backend.AnalysisSystem.Services.File.ReportFileService;
+import com.gabrielyget.challenge.backend.AnalysisSystem.Services.File.SplitFileService;
+import com.gabrielyget.challenge.backend.AnalysisSystem.Services.Watch.WatchService;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class AnalysisSystemApplication {
 	private static Dotenv dotenv = Dotenv.load();
 	private static String folderIn = dotenv.get("app.folder.in.path");
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		SpringApplication.run(AnalysisSystemApplication.class, args);
-		// TODO app who watch this service for restart or call for some help if this is down
 
 		ReportFileService reportFileService = new ReportFileService(
 				new CustomerService(new SplitFileService()),
 				new SalesmanService(new SplitFileService()),
-				new SaleService(new SplitFileService()),
-				new SplitFileService()
+				new SaleService(new SplitFileService())
 		);
-
-		System.out.println("string path parameter on main reportallfiles/watchfolder : " + folderIn);
 		reportFileService.generateReportOfAllFilesInFolder(folderIn);
 
 		new WatchService(
